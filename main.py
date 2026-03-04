6ad4b902146499ccf8da1c44be9ddbeac53b1d63
Bogdan <Bogdan@>
2026-03-04 13:16:41 +0500

    bogdan1new

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from proekt640AxmedovDilmurod import funk640DILMUROD
+from funcKosyanenko import funk640BOGDAN
 
 y = int(input("Введите число y: "))
 funk640DILMUROD(y)
+
+
+
+x = int(input("Enter your x"))
+funk640BOGDAN(x)
