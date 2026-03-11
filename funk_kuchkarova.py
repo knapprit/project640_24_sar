77892fb17e92490a0ada51e9df1d2da7bae10485
Sarvinoz Kuchkarova <kerayasti@gmail.com>
2026-03-11 13:53:17 +0500

    я н в чём не виноград

---
+++ funk_kuchkarova.py
@@ -0,0 +1,4 @@
+import math
+
+def tg_x(x):
+    return math.tan(math.radians(x))
--- main.py
+++ main.py
@@ -8,3 +8,14 @@ funk640DILMUROD(y)
 
 x = int(input("Enter your x"))
 funk640BOGDAN(x)
+
+
+###
+
+from funk_kuchkarova import tg_x
+
+x = float(input("Введите угол: "))
+
+result = tg_x(x)
+
+print("tg(", x, ") =", result)
